f71ea41a7754d8e4d468254acf3872b36a56e756
Andrew <aknittle623@gmail.com>
2019-03-24 01:13:10 -0400

    Added API Key
    Basic implementation of Google Maps

--- data_polling.py
+++ data_polling.py
@@ -1,10 +1,28 @@
 """Python Module used for """
+import googlemaps
+import gmplot
+from datetime import datetime
+
+
+api_file = open("api_key.txt", "r")
+api_key = api_file.read()
+
 
 def poll_data():
-    data_list = []
+    gmap = googlemaps.Client(key=api_key)
+    geocode_result = gmap.geocode("First St SE, Washington, DC 20004")
+    print(geocode_result)
+    now = datetime.now()
+    directions_result = gmap.directions("Washington Monument",
+                                        "United States Capitol",
+                                        mode="transit",
+                                        departure_time=now)
+    print(directions_result)
     return
 
+
 def main():
+    poll_data()
     return
 
 
